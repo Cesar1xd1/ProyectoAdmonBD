a2af99222ec178c1d4d307ffe69ab0135e72e8d5
Cesar Ramirez <cesar1xd1@gmail.com>
2022-06-20 14:00:21 -0500

    se añadio configuracion a ventana princilap y se comenzo a diseñar la ventana de Empleados

--- src/ventanas/VentanaPrincipal.java
+++ src/ventanas/VentanaPrincipal.java
@@ -26,21 +26,61 @@ public class VentanaPrincipal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenuEmpleados = new javax.swing.JMenu();
+        jMenuDepartamentos = new javax.swing.JMenu();
+        jMenuAdministradores = new javax.swing.JMenu();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        jPanel1.setBackground(new java.awt.Color(102, 204, 255));
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 681, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 380, Short.MAX_VALUE)
+        );
+
+        jMenuEmpleados.setText("Empleados");
+        jMenuEmpleados.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jMenuEmpleadosMouseClicked(evt);
+            }
+        });
+        jMenuBar1.add(jMenuEmpleados);
+
+        jMenuDepartamentos.setText("Departamentos");
+        jMenuBar1.add(jMenuDepartamentos);
+
+        jMenuAdministradores.setText("Administradores");
+        jMenuBar1.add(jMenuAdministradores);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jMenuEmpleadosMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenuEmpleadosMouseClicked
+        new VentanaEmpleados().setVisible(true); 
+    }//GEN-LAST:event_jMenuEmpleadosMouseClicked
 
     /**
      * @param args the command line arguments
@@ -78,5 +118,10 @@ public class VentanaPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenu jMenuAdministradores;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenu jMenuDepartamentos;
+    private javax.swing.JMenu jMenuEmpleados;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
